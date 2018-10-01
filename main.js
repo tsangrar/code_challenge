98d18dc224c00b2293845d0611142e9434a4bac0
tsangrar <37652316+tsangrar@users.noreply.github.com>
2018-10-01 17:41:01 +0530

    Changes to JS functions and less changes

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
+
 	/* Variable declarations */
-	var product_name;
-	var product_image;
-	var product_price;
+
+	var product_details = {
+		product_name: "",
+		product_image: "",
+		product_price: ""
+	};
 	var cart_p_name, cart_p_id;
-	var divtext;
-	var titletext;
-	var img_text;
-	var price_text;
+	var newproduct;
+	var cartproduct;
 	/* JSON Data into a JavaScript array for data manipulation*/
+
 	var products_data = [
 		{
 			"isPublished": "true",
@@ -51,18 +54,18 @@ $(document).ready(function () {
 	/*For loop to get the product name, product image src, and product price*/
 
 	for (i = 0; i < products_data.length; i++) {
-		if ((products_data[i]['isPublished']) == "true") {
-			product_name = products_data[i]['productName'];
-			product_image = products_data[i]['productImage'];
-			product_price = products_data[i]['price'];
-			createProducts(i, product_name, product_image, product_price);
+		if ((products_data[i]['isPublished']) === "true") {
+			product_details.product_name = products_data[i]['productName'];
+			product_details.product_image = products_data[i]['productImage'];
+			product_details.product_price = products_data[i]['price'];
+			createProducts(i, product_details);
 		}
 	}
 
 	/*Function on click of the button "Add To Cart"*/
 
 	$(".cart_add").click(function () {
-		var cart_pro_name = $(this).parent('.product').find('.product_name').text();
+		var cart_pro_name = $(this).parent('.product').find('.product_title').text();
 		var cart_pro_id = $(this).parent().attr('id');
 		$(this).parent('.product').hide();
 		addcart(cart_pro_name, cart_pro_id);
@@ -73,28 +76,29 @@ $(document).ready(function () {
 
 /*Function to create the product divs using the JSON data*/
 
-function createProducts(j, p_name, p_image, p_price) {
-	var button = '<button type="button" class="btn cart_add">Add To Cart</button>';
-	var div_end = '</div>'
-	divtext = '<div class="product col-lg-3 col-lg-offset-1 col-md-12 col-sm-12 col-xs-8" id="pro_' + j + '">';
-	titletext = '<h4 class="product_name">' + p_name + '</h4>';
-	img_text = '<img src="' + p_image + '"class="product_img">';
-	price_text = '<p class="product_price"> $' + p_price + '</p>';
-	$(".product_container").append(divtext + titletext + img_text + price_text + button + div_end);
+function createProducts(j, product_details) {
+	newproduct = '<div class="product col-lg-3 col-lg-offset-1 col-md-12 col-sm-10 col-sm-offset-2 col-xs-8" id="pro_' + j + '">';
+	newproduct += '<h4 class="product_title">' + product_details.product_name + '</h4>';
+	newproduct += '<img src="' + product_details.product_image + '"class="product_img">';
+	newproduct += '<p class="product_price"> $' + product_details.product_price + '</p>';
+	newproduct += '<button type="button" class="btn cart_add">Add To Cart</button>';
+	newproduct += '</div>';
+	$(".product_container").append(newproduct);
 }
 
 /*Function to add the product to the shopping cart*/
 
 function addcart(cart_p_name, cart_p_id) {
-	var divcart = '<div class="product_cart">';
-	var divcartname = '<span class="product_cart_name">' + cart_p_name + '</span>';
-	var cart_btn = '<button type="button" class="btn cart_remove" id="' + cart_p_id + '">Remove</button>';
-	var divcart_end = '</div>';
-	$(".cart").append(divcart + divcartname + cart_btn + divcart_end);
-		/*Function to remove the products from the shopping cart and back to the products list*/
-		$(".cart_remove").click(function () {
-			$(this).parent('.product_cart').hide();
-			var showElem = $(this).attr('id');
-			$("#" + showElem).show();
-		});
+	cartproduct = '<div class="product_cart">';
+	cartproduct += '<span class="product_cart_name">' + cart_p_name + '</span>';
+	cartproduct += '<button type="button" class="btn cart_remove" id="' + cart_p_id + '">Remove</button>';
+	cartproduct += '</div>';
+	$(".cart").append(cartproduct);
+
 }
+/*Function to remove the products from the shopping cart and back to the products list*/
+$(".cart").on("click", ".cart_remove", function () {
+	$(this).parent('.product_cart').hide();
+	var showElem = $(this).attr('id');
+	$("#" + showElem).show();
+});
